e9320cba7fbbdf3596bb6e63dbf2b6e6126bcb6d
Alexandre Cosati <cosati.alexandre@gmail.com>
2018-05-21 20:30:24 -0300

    Ponto flutuante

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
 let numberDisplay = 0;
 let decimal = false;
+let tree = undefined;
 
 window.addEventListener('DOMContentLoaded', function () {
 	const display = document.querySelector('#display');
@@ -9,17 +10,35 @@ window.addEventListener('DOMContentLoaded', function () {
 function number(n) {
   numberDisplay = numberDisplay != 0 ? "" + numberDisplay + n : n;
   display.innerHTML = numberDisplay;
+	console.log(n + " pressed");
 }
 
 function addDecimal() {
-  numberDisplay = (decimal == false) ? numberDisplay + "." : numberDisplay;
+	console.log(!decimal ? "Decimal point" : "Oops! Can't do that!");
+  numberDisplay = ((!decimal) ? numberDisplay + "." : numberDisplay);
+	display.innerHTML = numberDisplay;
+	decimal = true;
 }
 
-function clear() {
+function clearDisplay() {
   decimal = false;
   numberDisplay = 0;
-  console.log(numberDisplay);
   display.innerHTML = numberDisplay;
+}
+
+function backspace() {
+	console.log(numberDisplay != 0 ? numberDisplay[numberDisplay.length-1] + " is no more..." : "This 0 is tough!");
+	numberDisplay = numberDisplay.length > 1 ? numberDisplay = numberDisplay.slice(0, -1) : numberDisplay = 0;
+	display.innerHTML = numberDisplay;
+}
+
+function addNode(operator) {
+	let node = {
+		operator,
+		left: numberDisplay,
+	}
+	if (tree === undefined) tree = node;
+	else
 }
 
 function operate() {
