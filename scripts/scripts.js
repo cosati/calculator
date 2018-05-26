0652f9360444ef7012c765dc56500e7196df026a
Alexandre Cosati <cosati.alexandre@gmail.com>
2018-05-25 23:04:20 -0300

    Correções e implementações

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,50 +1,43 @@
-let current = 0;
-let array = [];
+let current = ""; // last number on display
+let array = []; // operations
+let pristine = true; // true when display cleared and after equals pressed
 
 window.addEventListener('DOMContentLoaded', function () {
 	const display = document.querySelector('#display');
-	createNode('sum', 0, false);
 	displayNumbers();
 });
 
 // Add number to display
-function pressButton(n) {
-	// TODO press operator before number
-	// TODO press minus before anything else
-	if (current != 0) current += "" + n; 
-	else current = n;
+function number(n) {
+	// zeros neither to the left nor simultanious
+	current = (pristine || current == 0 ? n : current + n);
+	// Pristine: Pressing a number after displaying a result
+	// should erase the result and display only the pressed number
+	pristine = false;
 	displayNumbers();
 }
 
-function pressOperator(o) {
-	// TODO press different operators simultaniously
-	let operator;
-	switch (o) {
-		case "sub":
-			operator = " - ";
-			break;
-		case "mul":
-			operator = " × ";
-			break;
-		case "div":
-			operator = " ÷ ";
-			break;
-		default:
-			operator = " + "
+function pressOperator(operator) {
+	let l = array.length;
+	let lastOperator = l > 0 ? array[l-1].operator : '';
+	let lastNumber = l > 0 ? array[l-1].number : '';
+	// if first pressed button is '-', first number will be negative. To correct, press clear
+	if (l != 1 || lastOperator != 'sub' || lastNumber !== '' || current !== '') {
+		// press operator before pressing any number
+		if (l == 0 && current === "") createNode(operator, "", false);
+		// changes last index's operator if operators pressed simultaniously
+		else if (current === "") array[l-1].operator = operator;
+		else createNode(operator, current, false);
+		current = "";
 	}
-	// array[array.length-1].operator = o;
-	// array[array.length-1].number = current;
-	createNode(o, current, false);
-	current = "";
 	displayNumbers();
-	current = 0;
-	// Add another node to array
 }
 
+// Adds a number and operator do the array
 function createNode(operator, number, isDecimal) {
 	let node = {
-		operator, 
-		number, 
+		operator,
+		number,
 		isDecimal,
 	}
 	array.push(node);
@@ -52,52 +45,56 @@ function createNode(operator, number, isDecimal) {
 
 // Display numbers on the screen
 function displayNumbers() {
-	numberDisplay = "";
-	let op = "";
-	for (let i = 0; i < array.length; i++) {
-		switch (array[i].operator) {
-			case 'sum':
-				op = " + ";
-				break;
-			case 'sub':
-				op = " - ";
-				break;
-			case 'mul':
-				op = " × ";
-				break;
-			case 'div':
-				op = " ÷ ";
-				break;
+	// TODO ... for larger numbers
+	let numberDisplay = "";
+	if (array.length > 0 || current != "") {
+		let op = "";
+		for (let i = 0; i < array.length; i++) {
+			switch (array[i].operator) {
+				case 'sum':
+					op = " + ";
+					break;
+				case 'sub':
+					op = (i > 0 || array[i].number !== '' ? " - " : "-");
+					break;
+				case 'mul':
+					op = " × ";
+					break;
+				case 'div':
+					op = " ÷ ";
+					break;
+				case undefined:
+					op = "";
+					break;
+			}
+			numberDisplay += array[i].number + op;
 		}
-		// TODO fix first number possibilities
-		numberDisplay += array[i].number + op;
+		numberDisplay += current;
 	}
-	numberDisplay += current;
+	else numberDisplay = 0; // Fresh start
+	if (numberDisplay.length > 10) {
+		numberDisplay = numberDisplay.slice(0, 9);
+		numberDisplay += '...';
+	}
 	display.innerHTML = numberDisplay;
 }
 
+// Fresh start
 function clearDisplay() {
-  createNode(undefined, 0, false);
-  current = 0;
-  display.innerHTML = 0;
-}
-
-function backspace() {
-	console.log(numberDisplay != 0 ? numberDisplay[numberDisplay.length-1] + " is no more..." : "This 0 is tough!");
-	numberDisplay = numberDisplay.length > 1 ? numberDisplay = numberDisplay.slice(0, -1) : numberDisplay = 0;
-	display.innerHTML = numberDisplay;
+  current = "";
+	array = [];
+	pristine = true;
+  displayNumbers();
 }
 
 function operate() {
-	// TODO order priority operation
+	// TODO order priority in operations
 	createNode(undefined, current, false);
-	//array[array.length-1].number = current;
-	let ans = array[0].number;
+	let ans = Number(array[0].number);
 	for (let i = 1; i < array.length; i++) {
 		switch (array[i-1].operator) {
 			case 'sum':
 				ans += Number(array[i].number);
-				console.log(ans);
 				break;
 			case 'sub':
 				ans -= Number(array[i].number);
@@ -113,11 +110,8 @@ function operate() {
 				break;
 		}
 	}
-	console.table(array);
-	current = 0;
+	current = ans;
+	pristine = true;
 	array = []
-	createNode('sum', ans, false);
 	displayNumbers();
 }
-
-//atualiza current - adiciona novo nó quando aperta operador
